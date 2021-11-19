0bf0a41006840fd3915acaf928c5e1983075537f
B-joyy <jbijoy97@gmail.com>
2021-11-20 01:45:42 +0530

    added functionality to delete button using JS

--- script.js
+++ script.js
@@ -3,36 +3,43 @@ var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
 function inputLength() {
-	return input.value.length;
+  return input.value.length;
 }
 
 function createListElement() {
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
-	input.value = "";
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(input.value));
+  ul.appendChild(li);
+  input.value = "";
 }
 
 function addListAfterClick() {
-	if (inputLength() > 0) {
-		createListElement();
-	}
+  if (inputLength() > 0) {
+    createListElement();
+  }
 }
 
 function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
-	}
+  if (inputLength() > 0 && event.keyCode === 13) {
+    createListElement();
+  }
+}
+
+function handleUlClick(e) {
+  if (e.target.tagName === "LI") toggleItemAfterClick(e);
+  if (e.target.className === "del-btn") deleteItemAfterClick(e);
 }
 
 function toggleItemAfterClick(e) {
-	if (e.target.tagName === "LI") {
-		e.target.classList.toggle("done");
-	}
+  e.target.classList.toggle("done");
+}
+
+function deleteItemAfterClick(e) {
+  e.target.parentElement.remove();
 }
 
 button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
 
-ul.addEventListener("click", toggleItemAfterClick);
+ul.addEventListener("click", handleUlClick);
